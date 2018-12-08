68d27558cc5de93b7158dd461c7e78dbfcb49a46
alexminer88 <alex.miner@gmail.com>
2018-12-07 21:29:40 -0800

    Implement cookieParser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,13 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+    var datastore = {};
+    // req.body.sessionID
+    // response, send back object with session data
+    console.log("req.body: ", req.body);
+    // models.Sessions.create();
+
+    
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,34 @@
+const utils = require('../lib/hashUtils.js'); 
+
 const parseCookies = (req, res, next) => {
+    // cookie = {id: }
+    var cookies = {};
+    // var cookies = req.headers;
+    console.log("HELLO THAR", req.headers);
+    // if cookie does not exist
+    if (req.headers.cookie === undefined) {
+        console.log("THIS IS WORKING ASDASD");
+        // create new session id and send back in response
+
+        let data = utils.createRandom32String();
+        let hash = utils.createHash(data);
+        req.cookies = {}; 
+        console.log("REQ COOKIE", req.cookies);
+    } else {
+        req.headers.cookie.split('; ').forEach((element)=>{
+            var arr = element.split('=');
+            console.log(arr);
+            cookies[arr[0]] = arr[1];
+            
+        });
+        console.log(cookies);
+        req.cookies = cookies;
+    }
+    // res.send(cookiessss);
+    next();
+        
+    // if it does exist
+        // compare to session id datastore and grab data
 };
 
 module.exports = parseCookies;
