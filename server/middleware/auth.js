f62c837dd2f82acab35eca8a23a303f9762a96a9
alexminer88 <alex.miner@gmail.com>
2018-12-07 22:01:53 -0800

    add auth cookies session

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,25 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const utils = require('../lib/hashUtils.js')
 
 module.exports.createSession = (req, res, next) => {
     var datastore = {};
     // req.body.sessionID
     // response, send back object with session data
-    console.log("req.body: ", req.body);
+    if (req.headers.cookies === undefined) {
+        let data = utils.createRandom32String();
+        let hash = utils.createHash(data);
+
+        req.session = {};
+        req.session.hash = hash;
+        res.cookies = {shortlyid: {value: 'asdasdas'}};
+    }
+    
+
+    console.log("response ASDASKDJLASD: ", res);
     // models.Sessions.create();
 
-    
+    next();
 };
 
 /************************************************************/
